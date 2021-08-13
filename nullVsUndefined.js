349a2f60e09c2e1277e97e6cbdae62f42979d9e5
FahmidaAz <66080543+FahmidaAz@users.noreply.github.com>
2021-08-13 13:45:33 -0400

    secondfile

--- nullVsUndefined.js
+++ nullVsUndefined.js
@@ -1,3 +1,24 @@
 let pakhi;
 console.log(pakhi);
 
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+
+const result =  add(80,100);
+console.log(result);
+
+
+function add2(num1, num2){
+    console.log(num1, num2);
+}
+
+const result2 = add2(12);
+console.log(result2);
+
+
+const lover ={name:"smart dude", phone:'234456743'};
+console.log(lover.gf);
+
+const age =[12,22,3];
+console.log(age[8]);
