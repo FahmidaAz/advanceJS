f372e38416ce87c3e62ddd9eb8838eb95ef30660
FahmidaAz <66080543+FahmidaAz@users.noreply.github.com>
2021-08-13 12:55:25 -0400

    done

---
+++ trueFalse.js
@@ -0,0 +1,9 @@
+const age = 0 //falsey
+const name ="" //falsey
+let name; //undefined
+let name = null //falsey
+let name = NaN // falsey
+const name = "0" //truthy
+const name = " "//truthy
+const name =[] //truthy
+const name ={} //truthy
